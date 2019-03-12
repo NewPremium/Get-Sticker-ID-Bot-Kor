ef0d6b22bbd238ba7790eb8e46a51c1d4f9d93bb
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2019-03-12 20:09:32 +0900

    한글화 완료

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ define('NAME', 'Get Sticker id');
 require './class.telegram.php';
 $bot = new Telegram(TOKEN, NAME);
 
-// Incoming update
+// 수신 업데이트
 $request = json_decode(file_get_contents('php://input'), true);
 if ($request['update_id'] && isset($request['message']['sticker']['file_id'])) {
-	// Message received
-	$bot->sendMessage($request['message']['chat']['id'], 'Hey '.$request['message']['from']['first_name'].', the Sticker ID you are asking for is: '.$request['message']['sticker']['file_id']);
+	// 수신된 메시지
+	$bot->sendMessage($request['message']['chat']['id'], '안녕하세요'.$request['message']['from']['first_name'].'님, the Sticker ID you are asking for is 당신이 보낸 스티커의 ID는 : '.$request['message']['sticker']['file_id'])'입니다.';
 }
 if ($_GET['setHook']) {
 	// uncomment, type in your webhook domain and visit example.com/path/to/bot/?setHook=1 to register the hook
